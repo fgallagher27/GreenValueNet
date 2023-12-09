d720dbe1c9770f318fdad84036c27e2dd3cf8781
fgallagher27 <felixgallagher@live.com>
2023-12-09 21:02:32 +0000

    isolate target vars

--- model_funcs.py
+++ model_funcs.py
@@ -3,6 +3,7 @@ This script contains the functions to run the baseline and deep neural network m
 """
 
 import pandas as pd
+from typing import List
 from sklearn.model_selection import train_test_split
 
 def split_to_test_dev_train(dataset: pd.DataFrame, dev_size, test_size, prop=True):
@@ -22,3 +23,21 @@ def split_to_test_dev_train(dataset: pd.DataFrame, dev_size, test_size, prop=Tru
     dev, test = train_test_split(remaining, test_size = (test_size / test_size + dev_size))
 
     return train, dev, test
+
+
+def extract_target_var(df:pd.DataFrame, target_col: str, out_cols: List[str]):
+    """
+    This function isolates the target variable as a seperate object
+    """
+    target = df[target_col]
+    input = df.drop(columns=[target_col, out_cols], axis=1)
+    return input, target
+
+
+def random_forest_reg():
+    """
+    This function creates the random forest regression
+    model used as a baseline model.
+    """
+
+    return model
