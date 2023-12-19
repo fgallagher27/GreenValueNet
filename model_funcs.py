6354cd0793d9ed9f3a509494c2796464f2a61ac2
fgallagher27 <felixgallagher@live.com>
2023-12-19 11:46:45 +0000

    inital neural net attempt

--- model_funcs.py
+++ model_funcs.py
@@ -5,7 +5,7 @@ This script contains the functions to run the baseline and deep neural network m
 import pandas as pd
 import numpy as np
 import tensorflow as tf
-from typing import List, Tuple
+from typing import List, Tuple, Union
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from tensorflow.keras import layers, models
@@ -105,11 +105,13 @@ def boosted_grad_reg(x_train, y_train) -> GradientBoostingRegressor:
 
 
 def neural_net(
-        x_train: tf.tensor,
-        y_train: tf.tensor,
+        x_train: Union[tf.Tensor, np.ndarray],
+        y_train: Union[tf.Tensor, np.ndarray],
+        n_hidden_units: int,
         n_layers: int = 1,
-        learning_rate: float = 0.01,
+        learning_rate: float = 0.05,
         batch_size: int = 32,
+        epochs: int = 50,
         hidden_activation: str = 'relu',
         output_activation: str = 'linear',
         loss: str = 'mean_squared_error'
@@ -124,21 +126,21 @@ def neural_net(
     
     # Adding hidden layers
     for _ in range(n_layers):
-        model.add(layers.Dense(128, activation=hidden_activation))
+        model.add(layers.Dense(n_hidden_units, activation=hidden_activation))
     # Adding output layer
     model.add(layers.Dense(units=1, activation=output_activation))
     
     model.compile(
-        optimizer=tf._optimizers.Adam(learning_rate = learning_rate),
+        optimizer=tf._optimizers.Adam(learning_rate=learning_rate),
         loss=loss,
         metrics=[loss]
     )
-    model.fit(x_train, y_train, epochs=5, batch_size=batch_size) 
+    model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size) 
 
     return model
 
 
-def generate_pred_metric(model, metric: function, x_dev, y_dev):
+def generate_pred_metric(model, metric, x_dev, y_dev):
     """
     This function takes model which has a method predict
     and generates predictions and accuracy according to a 
